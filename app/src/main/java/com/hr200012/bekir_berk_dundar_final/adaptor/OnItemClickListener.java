58ab59b266aec150542257dd490ccdc9ae592329
bekirberkdundar2 <97554911+bekirberkdundar2@users.noreply.github.com>
2022-01-29 15:49:44 +0300

    Delete app directory

--- app/src/main/java/com/hr200012/bekir_berk_dundar_final/adaptor/OnItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.hr200012.bekir_berk_dundar_final.adaptor;
-
-import com.hr200012.bekir_berk_dundar_final.model.Futbolcu;
-
-public interface OnItemClickListener {
-    void onItemClick(Futbolcu data);
-}
--- app/src/main/java/com/hr200012/bekir_berk_dundar_final/util/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.hr200012.bekir_berk_dundar_final.util;
-
-public class Constants {
-    public static String BASE_URL = "https://raw.githubusercontent.com/bekirberkdundar2/hr200012bekirberkdundar/main/";
-}
